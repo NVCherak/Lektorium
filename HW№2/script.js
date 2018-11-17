03d16beed7eb8c41780bd67292253f54e4cb5257
Nazar Cherak <nvcherak@gmail.com>
2018-11-17 13:41:52 +0200

    Fix Home work 2

--- HW№2/script.js
+++ HW№2/script.js
@@ -5,17 +5,25 @@ function doWithArray(array, key) {
             for (var i in array) {
                 if (array[i] > tmp) tmp = array[i];
             }
-            return tmp;
+            break;
         case 'min':
             for (var i in array) {
                 if (array[i] < tmp) tmp = array[i];
             }
-            return tmp;
+            break;
         case 'sum':
             tmp = 0;
             for (var i in array) {
+                if (typeof array[i] == "undefined" || isNaN(array[i])) {  //if number is undefined or has type NaN - skip iteration
+                    continue;
+                }
                 tmp += array[i];
             }
-            return tmp;
+            break;
     }
+
+    return tmp;
 }
+
+
+console.log(doWithArray([1, undefined, NaN, 5, -2,0],'max'));
