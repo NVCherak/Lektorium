8e66be6e93ea7b5081b4de102c877cad2113107b
Nazar Cherak <nvcherak@gmail.com>
2019-03-11 18:02:19 +0200

    Homework №3

--- HW№3/scripts.js
+++ HW№3/scripts.js
@@ -1,25 +1,36 @@
 function calculateCapacity(array) {
-    let sumOfCapacity = 0;
+    let sumOfCapacity = 0
 
     walksAnArray:
         for (let i = 1; i < array.length; i++) {
             if (array[i-1] > array[i]) {
-                let tmpCapacity = 0;
+                let tmpCapacity = 0
                 for (let j = i; j < array.length; j++) {
                     if (array[i-1] > array[j]) {
                         tmpCapacity += array[i-1] - array[j];
                     } else {
                         sumOfCapacity += tmpCapacity;
-                        i = j;
-                        break;
+                        i = j
+                        break
                     }
                     if (j+1 == array.length) {
-                        sumOfCapacity += calculateCapacity(array.slice(i-1).reverse());
-                        break walksAnArray;
+                        sumOfCapacity += calculateCapacity(array.slice(i-1).reverse())
+                        break walksAnArray
                     }
                 }
             }
         }
 
-    return sumOfCapacity;
+    return sumOfCapacity
+}
+
+
+function arrayToObj(array) {
+    let obj = {}
+
+    for (let i = 0; i < array.length; i++) {
+        obj[`${array[i]}`] = i
+    }
+
+    return obj
 }
